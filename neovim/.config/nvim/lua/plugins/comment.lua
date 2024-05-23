a97f6f063501f25cf5062266ed0b2c4e11500e46
Jerry Wang <jianjunw@cn.ibm.com>
2024-05-23 20:01:10 +0800

    update

--- neovim/.config/nvim/lua/plugins/comment.lua
+++ neovim/.config/nvim/lua/plugins/comment.lua
@@ -1,5 +1,8 @@
+local vim = vim
+local enabled = vim.version().minor < 10 and true or false
 return {
 	"numToStr/Comment.nvim",
+	enabled = enabled,
 	config = function()
 		require("Comment").setup()
 	end,
--- neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -4,12 +4,13 @@ local version = vim.version()
 local minor = version.minor
 -- local tag = string.format("v0.%s.0", minor)
 local tag = "v0.8.0"
+local enabled = vim.version().minor < 10 and true or false
 
 return {
 	"nvim-treesitter/nvim-treesitter",
-	enabled = false,
+	enabled = enabled,
 	build = ":TSUpdate",
-  tag = tag,
+	tag = tag,
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
